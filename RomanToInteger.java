ad51ab43e80dae4c061952dc97ea15d7ac35366b
Renuka kanade <86623689+06Renuka@users.noreply.github.com>
2023-11-08 22:44:22 +0530

    Add files via upload
    Q.1 C Check if the input is pangram or not. (Pangram is a sentence that contains all the alphabet
    from a-z)(java Assignment)

---
+++ RomanToInteger.java
@@ -0,0 +1,51 @@
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int result = 0;
+        int previousValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = 0;
+
+            switch (currentChar) {
+                case 'I':
+                    currentValue = 1;
+                    break;
+                case 'V':
+                    currentValue = 5;
+                    break;
+                case 'X':
+                    currentValue = 10;
+                    break;
+                case 'L':
+                    currentValue = 50;
+                    break;
+                case 'C':
+                    currentValue = 100;
+                    break;
+                case 'D':
+                    currentValue = 500;
+                    break;
+                case 'M':
+                    currentValue = 1000;
+                    break;
+            }
+
+            if (currentValue < previousValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            previousValue = currentValue;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String roman = "IX"; // Example: Roman numeral "IX"
+        int result = romanToInt(roman);
+        System.out.println("Roman to Integer: " + result);
+    }
+}
